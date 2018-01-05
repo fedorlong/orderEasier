110f501d98119df41bb3650c785d3691882c0ba5
longtao <long.tao@puscene.com>
2018-01-05 18:31:32 +0800

    uo

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,11 +1,19 @@
 ;jQuery(function($) {
-  var btnIntro = $('#introduce');
 
   // initialize subNav to hide status
   $('#subNav-introduce').hide();
+  $('#subNav-lesson').hide();
 
   // click navItem to toggle subNav
+  var btnIntro  = $('#introduce'),
+      btnLesson = $('#lesson');
+
   btnIntro.on('click', function(e) {
-    $('#subNav-introduce').slideToggle();
+    $('#subNav-introduce').slideToggle('fast');
   });
+
+  btnLesson.on('click', function(e) {
+    $('#subNav-lesson').slideToggle('fast');
+  });
+
 });
