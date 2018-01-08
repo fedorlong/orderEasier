8bb527f163f4cc1d6feed8b732a1d79f7fad5b54
longtao <long.tao@puscene.com>
2018-01-08 11:50:40 +0800

    up

--- public/javascripts/interact.js
+++ public/javascripts/interact.js
@@ -18,7 +18,8 @@
 });
 
 /**
- * hide subNavigators when initialize the page
+ * hide subNavigators when initialize the page,
+ * aim to give a slideDown effect when first show subNav
  * @return {*} [description]
  */
 function hideSubNavs() {
@@ -28,7 +29,8 @@ function hideSubNavs() {
 
 
 /**
- * navigator item listening click event, item`s subNav toggle when item clicked
+ * navigator item listening click event,
+ * item`s subNav toggle when item clicked
  * @param  {jQuery object} btn       the navigator item
  * @param  {jQuery object} subNav    the subNav related to item
  * @param  {string} flagClass        the class record hideStatus
@@ -38,13 +40,34 @@ function navBtnListeningClick(btn, subNav, flagClass) {
 
   btn.on('click', function(e) {
 
+    hideOtherSubNav(subNav, flagClass);
+
     if (subNav.hasClass(flagClass)) {
-      subNav.removeClass(flagClass);
-      subNav.slideDown('fast');
+      subNav.removeClass(flagClass).slideDown('fast');
 
     } else {
-      subNav.slideUp('fast');
-      subNav.addClass(flagClass);
+      subNav.slideUp('fast').addClass(flagClass);
     }
   });
+}
+
+/**
+ * hide other subNavigators before click current btn
+ * @param  {jQuery object} currentSubNav [description]
+ * @param  {string} flagClass     the class record hideStatus
+ * @return {[type]}               [description]
+ */
+function hideOtherSubNav(currentSubNav, flagClass) {
+  var subNavs = $('.subNav-item');
+
+  for (var index=0; index < subNavs.length; index++) {
+
+    // skip self
+    if (subNavs[index] === currentSubNav[0]) { continue; }
+
+    if (!subNavs.eq(index).hasClass(flagClass)) {
+      subNavs.eq(index).slideUp('fast').addClass(flagClass);
+    }
+  }
+
 }
